1d07b0b48e4a41d8db94ce4e0c824f42dc836f8a
Beau McCartney <mccartney.beausl@gmail.com>
2021-11-03 17:18:08 -0600

    Fix: comparisons and access modifiers in sorts

--- exA/SelectionSorter.java
+++ exA/SelectionSorter.java
@@ -3,14 +3,14 @@ import java.util.ArrayList;
 //selection sort - selects the smallest element for the current slot and swaps, iterates left to right
 public class SelectionSorter<E extends Number & Comparable<E>> implements Sorter<E> {
     @Override
-    void sort(ArrayList<Item<E>> arr) {
+    public void sort(ArrayList<Item<E>> arr) {
         //iterates all but the last value
         for(int i = 0; i < arr.size() - 1; i++) {
             int min = i; //index of smallest item
             //finds the smallest item past i
             for(int j = i + 1; j < arr.size(); j++) {
-                if(arr.get(j).getItem() < arr.get(min).getItem())
-                    index = j;
+                if(arr.get(j).getItem().compareTo(arr.get(min).getItem()) < 0)
+                    min = j; // where do i declare???
             }
             //swaps smallest index and current index items
             Item<E> temp = arr.get(i);
--- exA/Sorter.java
+++ exA/Sorter.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
 
 public interface Sorter <E extends Number & Comparable<E>> {
-    void sort(ArrayList<Item<E>> arr); // diff return type?
+    public void sort(ArrayList<Item<E>> arr); // diff return type?
 }
