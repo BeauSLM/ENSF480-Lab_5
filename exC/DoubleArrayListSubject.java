6d54f0add0012b34dd0b47587ba000b907651322
Beau McCartney <mccartney.beausl@gmail.com>
2021-11-03 23:48:29 -0600

    Document the subject class

--- exC/DoubleArrayListSubject.java
+++ exC/DoubleArrayListSubject.java
@@ -2,42 +2,51 @@ import java.util.ArrayList;
 import java.util.Observer;
 
 public class DoubleArrayListSubject implements Subject {
-    public ArrayList<Double> data; // how to make visible to observer?
-    private ArrayList<Observer> observers;
+    public ArrayList<Double> data; // our data
+    private ArrayList<Observer> observers; // observers of this subject
 
+    // initializes data members to new ArrayLists<>
     public DoubleArrayListSubject() {
         data = new ArrayList<>();
         observers = new ArrayList<>();
     }
 
+    // adds a data point, notifies observers
     public void addData(double d) {
         this.data.add(d);
         notifyAllObservers();
     }
 
+    // adds a data point, notifies observers
     public void setData(int index, double d) {
         data.set(index, d);
         notifyAllObservers();
     }
 
+    // populates data with an array, notifies all observers
     public void populate(double [] arr) {
         data = new ArrayList<>(arr);
+        notifyAllObservers();
     }
 
+    // adds an observer to the array
     public void registerObserver(Observer o) {
         observers.add(o);
     }
 
+    // removes an observer from the array
     public void removeObserver(Observer o) {
         observers.remove(o);
     }
 
+    // notifies all observers
     public void notifyAllObservers() {
         for (Observer o : observers) {
             o.update();
         }
     }
 
+    // observes all display methods
     public void display() {
         if (observers.isEmpty()) {
             System.err.println("Empty List ...");
